0c18abe2de0e0a9ce1c593876ad4477f0e3e3441
Valerii <badday182@gmail.com>
2024-03-30 11:42:24 +0200

    closing modal window works

--- client/src/components/modalWindows/ListOptionsModal.tsx
+++ client/src/components/modalWindows/ListOptionsModal.tsx
@@ -1,9 +1,9 @@
 import { FC, useEffect, useRef, useState } from "react";
-
 import { IListOptionsModalProps, ILists } from "../../types/types";
 import { AiFillEdit } from "react-icons/ai";
 import { MdDeleteForever } from "react-icons/md";
 import { instance } from "../../api/axios.api";
+import { IoCloseSharp } from "react-icons/io5";
 
 const ListOptionsModal: FC<IListOptionsModalProps> = ({
   listId,
@@ -30,15 +30,12 @@ const ListOptionsModal: FC<IListOptionsModalProps> = ({
     return () => {
       window.removeEventListener("resize", handleResize);
     };
-
-    
   }, []);
 
- 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await instance.get('/lists');
+        const response = await instance.get("/lists");
         setLists(response.data);
       } catch (error) {
         console.error("Error fetching lists:", error);
@@ -48,9 +45,11 @@ const ListOptionsModal: FC<IListOptionsModalProps> = ({
     fetchData();
   }, []);
 
+  const handleCloseModal = () => {
+    onClose(); // Закрытие модального окна
+  };
 
   const handleDeleteList = async () => {
-    
     console.log("Deleting list with ID:", listId);
 
     try {
@@ -58,7 +57,6 @@ const ListOptionsModal: FC<IListOptionsModalProps> = ({
       console.log("List deleted successfully");
       onClose();
       updateListsAfterDelete(); // Обновление списка после удаления
-
     } catch (error) {
       console.error("Error deleting list:", error);
     }
@@ -71,19 +69,23 @@ const ListOptionsModal: FC<IListOptionsModalProps> = ({
   return (
     <div
       ref={modalRef}
-      className={`fixed inset-0 flex items-center justify-center z-50 ${
+      className={` fixed inset-0 flex items-center justify-center z-50 ${
         visible ? "block" : "hidden"
       }`}
     >
       <div
-        className="absolute bg-slate-800 shadow-lg rounded-lg p-4 flex flex-col  items-start"
+        className="absolute border-gray-500 border-2 bg-slate-800 shadow-lg rounded-lg p-4 flex flex-col  items-start"
         style={modalStyle}
       >
+         <button className="btn-close rounded-full absolute top-1 right-1" onClick={handleCloseModal}>
+          <IoCloseSharp size={15} />
+        </button>
         <button className="btn flex items-center justify-center">
           <AiFillEdit className="mr-2" size={18} />
-          Edit List
+          Edit Title  
         </button>
-        <button ref={deleteButtonRef}
+        <button
+          ref={deleteButtonRef}
           onClick={handleDeleteList}
           className="btn btn-red mt-2 flex items-center justify-center"
         >
